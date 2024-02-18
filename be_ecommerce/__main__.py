f3314afb4c8ee0d0e93d0263cd6a288064e56c75
AndreaMarras89 <andreamarras634@gmail.com>
2024-02-18 14:51:21 +0100

    added exception catching

--- be_ecommerce/__main__.py
+++ be_ecommerce/__main__.py
@@ -6,11 +6,12 @@ import gunicorn.app.base
 from gunicorn.glogging import Logger
 from loguru import logger
 
+from be_ecommerce.app import app
 from be_ecommerce.configuration import config
 
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
-    """Gunicorn Standalone Application."""
+    """Gunicorn application."""
 
     def __init__(self, app, options=None):
         """Inits."""
@@ -38,8 +39,6 @@ if __name__ == "__main__":
     parser.add_argument("--workers", type=int, default=config.default_workers)
     parser.add_argument("--bind", type=str, default=f"0.0.0.0:{config.server_port}")
     parser.add_argument("--timeout", type=str, default=str(config.default_timeout))
-    parser.add_argument("--local", dest="local", action="store_true")
-    parser.set_defaults(local=False)
     args = parser.parse_args()
 
     options = {
@@ -52,21 +51,4 @@ if __name__ == "__main__":
     }
 
     logger.info("Starting server")
-    from be_ecommerce.app import app
-
-    if args.local:
-        logger.info("Starting server uvicorn")
-        import uvicorn
-
-        _, port = args.bind.split(":")
-        uvicorn.run(
-            "be_ecommerce.app:app",
-            host="0.0.0.0",
-            port=config.server_port,
-            log_level=config.log_level,
-            reload=True,
-            workers=config.default_workers,
-        )
-    else:
-        logger.info("Starting server gunicorn")
-        StandaloneApplication(app, options).run()
+    StandaloneApplication(app, options).run()
--- be_ecommerce/api/routers.py
+++ be_ecommerce/api/routers.py
@@ -20,9 +20,10 @@ from be_ecommerce.db.utils import DatabaseSessionMaker
 router = APIRouter()  # invoco costruttore default
 session_maker = DatabaseSessionMaker()
 
+
 async def check_user_exists(user_id: str) -> bool:
     """Utility function to check if the user_id exists in the database"""
-    
+
     async with session_maker.get_session() as session:
         my_query = select(User).where(user_id == User.uid)
         result = await session.execute(my_query)
@@ -34,7 +35,7 @@ async def check_user_exists(user_id: str) -> bool:
 
 async def check_product_exists(product_id: str) -> bool:
     """Utility function to check if the product_id exists in the database"""
-    
+
     async with session_maker.get_session() as session:
         my_query = select(ProductTable.id).where(ProductTable.id == product_id)
         result = await session.execute(my_query)
@@ -45,38 +46,137 @@ async def check_product_exists(product_id: str) -> bool:
 
 
 @router.post("/search")
-async def search_product(payload: ProductSearchInput) -> dict:
+async def search_product(payload: ProductSearchInput) -> ProductSearchOutput:
     """Logic of the search endpoint"""
-    
-    async with session_maker.get_session() as session:
-        my_sql = select(ProductTable).where(
-            or_(
-                ProductTable.name.ilike(f"%{payload.query}%"),
-                ProductTable.description.ilike(f"%{payload.query}%"),
-            )
-        )
-        result = await session.execute(my_sql)
-        result_list: list = []
-        for record in result.fetchall():
-            result_list.append(
-                Product(
-                    id=str(record[0].id),
-                    name=record[0].name,
-                    price=record[0].price,
-                    description=record[0].description,
+
+    try:
+        async with session_maker.get_session() as session:
+            my_sql = select(ProductTable).where(
+                or_(
+                    ProductTable.name.ilike(f"%{payload.query}%"),
+                    ProductTable.description.ilike(f"%{payload.query}%"),
                 )
             )
-        return {"products": result_list}
+            result = await session.execute(my_sql)
+            result_list: list = []
+            for record in result.fetchall():
+                result_list.append(
+                    Product(
+                        id=str(record[0].id),
+                        name=record[0].name,
+                        price=record[0].price,
+                        description=record[0].description,
+                    )
+                )
+            return ProductSearchOutput(products=result_list)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
 
 
 @router.post("/add_product")
 async def add_to_cart(payload: ProductAddCartInput) -> ProductAddCartOutput:
     """Logic of the add_product endpoint"""
-    
-    async with session_maker.get_session() as session:
-        if await check_user_exists(payload.user_id) and await check_product_exists(
-            payload.product_id
-        ):
+
+    try:
+        async with session_maker.get_session() as session:
+            if await check_user_exists(payload.user_id) and await check_product_exists(
+                payload.product_id
+            ):
+                my_sql = select(UserCart).where(
+                    and_(
+                        UserCart.uid == payload.user_id,
+                        UserCart.pid == payload.product_id,
+                    )
+                )
+                result = await session.execute(my_sql)
+                record = result.first()
+                if record and len(record) > 0:
+                    my_sql = (
+                        update(UserCart)
+                        .where(
+                            and_(
+                                UserCart.pid == payload.product_id,
+                                UserCart.uid == payload.user_id,
+                            )
+                        )
+                        .values(quantity=record[0].quantity + payload.quantity)
+                    )
+                    compile = my_sql.compile()
+                    result = await session.execute(my_sql)
+                    await session.commit()
+                else:
+                    my_query = insert(UserCart).values(
+                        pid=payload.product_id,
+                        uid=payload.user_id,
+                        quantity=payload.quantity,
+                    )
+                    compile = my_query.compile()
+                    result = await session.execute(my_query)
+                    await session.commit()
+            else:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Product ID or UserID not found in the database",
+                )
+        return ProductAddCartOutput(added=True)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
+
+
+@router.post("/product_details")
+async def product_details(payload: ProductDetailsInput) -> ProductDetailsOutput:
+    """Logic of the product_details endpoint"""
+
+    try:
+        async with session_maker.get_session() as session:
+            my_sql = select(ProductTable).where(ProductTable.id == payload.product_id)
+            result = await session.execute(my_sql)
+            record = result.first()
+            if record and len(record) > 0:
+                return ProductDetailsOutput(
+                    product_name=record[0].name,
+                    product_description=record[0].description,
+                    product_price=record[0].price,
+                )
+            else:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"product with ID {payload.product_id} not found",
+                )
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
+
+
+@router.post("/product_removal_all")
+async def product_removal_all(
+    payload: ProductRemovalAllInput,
+) -> ProductRemovalAllOutput:
+    """Logic of the product_removal_all endpoint"""
+
+    try:
+        if not check_user_exists(payload.user_id):
+            raise HTTPException(status_code=404, detail="User not found")
+
+        async with session_maker.get_session() as session:
+            my_sql = delete(UserCart).where(UserCart.uid == payload.user_id)
+            compile = my_sql.compile()
+            result = await session.execute(my_sql)
+            await session.commit()
+            return ProductRemovalAllOutput(removed=True)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
+
+
+@router.post("/product_removal_quantity")
+async def product_removal_quantity(
+    payload: ProductRemovalWithQuantityInput,
+) -> ProductRemovalWithQuantityOutput:
+    """Logic of the product_removal_quantity endpoint"""
+
+    try:
+        if not check_user_exists(payload.user_id):
+            raise HTTPException(status_code=404, detail="User not found")
+        async with session_maker.get_session() as session:
             my_sql = select(UserCart).where(
                 and_(
                     UserCart.uid == payload.user_id, UserCart.pid == payload.product_id
@@ -85,111 +185,31 @@ async def add_to_cart(payload: ProductAddCartInput) -> ProductAddCartOutput:
             result = await session.execute(my_sql)
             record = result.first()
             if record and len(record) > 0:
-                my_sql = (
-                    update(UserCart)
-                    .where(
+                quantity = record[0].quantity - payload.quantity
+                if quantity <= 0:
+                    quantity = 0
+                    my_query = delete(UserCart).where(
                         and_(
-                            UserCart.pid == payload.product_id,
                             UserCart.uid == payload.user_id,
+                            UserCart.pid == payload.product_id,
                         )
                     )
-                    .values(quantity=record[0].quantity + payload.quantity)
-                )
-                compile = my_sql.compile()
-                result = await session.execute(my_sql)
-                await session.commit()
-            else:
-                my_query = insert(UserCart).values(
-                    pid=payload.product_id,
-                    uid=payload.user_id,
-                    quantity=payload.quantity,
-                )
+                else:
+                    my_query = (
+                        update(UserCart)
+                        .where(
+                            and_(
+                                UserCart.uid == payload.user_id,
+                                UserCart.pid == payload.product_id,
+                            )
+                        )
+                        .values(quantity=quantity)
+                    )
                 compile = my_query.compile()
                 result = await session.execute(my_query)
                 await session.commit()
-        else:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Product ID or UserID not found in the database",
-            )
-    return {"added": True}
-
-
-@router.post("/product_details")
-async def product_details(payload: ProductDetailsInput) -> ProductDetailsOutput:
-    """Logic of the product_details endpoint"""
-    
-    async with session_maker.get_session() as session:
-        my_sql = select(ProductTable).where(ProductTable.id == payload.product_id)
-        result = await session.execute(my_sql)
-        record = result.first()
-        if record and len(record) > 0:
-            return ProductDetailsOutput(
-                product_name=record[0].name,
-                product_description=record[0].description,
-                product_price=record[0].price,
-            )
-        else:
-            raise HTTPException(
-                status_code=404,
-                detail=f"product with ID {payload.product_id} not found",
-            )
-
-
-@router.post("/product_removal_all")
-async def product_removal_all(
-    payload: ProductRemovalAllInput,
-) -> ProductRemovalAllOutput:
-    """Logic of the product_removal_all endpoint"""
-    if not check_user_exists(payload.user_id):
-        raise HTTPException(status_code=404, detail="User not found")
-    
-    async with session_maker.get_session() as session:
-        my_sql = delete(UserCart).where(UserCart.uid == payload.user_id)
-        compile = my_sql.compile()
-        result = await session.execute(my_sql)
-        await session.commit()
-        return {"removed": True}
-
-
-@router.post("/product_removal_quantity")
-async def product_removal_quantity(
-    payload: ProductRemovalWithQuantityInput,
-) -> ProductRemovalWithQuantityOutput:
-    """Logic of the product_removal_quantity endpoint"""
-    
-    if not check_user_exists(payload.user_id):
-        raise HTTPException(status_code=404, detail="User not found")
-    async with session_maker.get_session() as session:
-        my_sql = select(UserCart).where(
-            and_(UserCart.uid == payload.user_id, UserCart.pid == payload.product_id)
-        )
-        result = await session.execute(my_sql)
-        record = result.first()
-        if record and len(record) > 0:
-            quantity = record[0].quantity - payload.quantity
-            if quantity <= 0:
-                quantity = 0
-                my_query = delete(UserCart).where(
-                    and_(
-                        UserCart.uid == payload.user_id,
-                        UserCart.pid == payload.product_id,
-                    )
-                )
             else:
-                my_query = (
-                    update(UserCart)
-                    .where(
-                        and_(
-                            UserCart.uid == payload.user_id,
-                            UserCart.pid == payload.product_id,
-                        )
-                    )
-                    .values(quantity=quantity)
-                )
-            compile = my_query.compile()
-            result = await session.execute(my_query)
-            await session.commit()
-        else:
-            return {"removed": False}
-        return {"removed": True}
+                return ProductRemovalWithQuantityOutput(removed=False)
+            return ProductRemovalWithQuantityOutput(removed=True)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
